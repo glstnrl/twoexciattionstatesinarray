4aa57bb0750f25b74bccf59aea1346810f6a375c
The-LivingPlanet <avcie21@itu.edu.tr>
2026-05-19 22:23:07 +0300

    Add files via upload

---
+++ Angle.jl
@@ -0,0 +1,82 @@
+using QuantumOptics
+using LinearAlgebra
+using SparseArrays
+using Plots
+
+N=3
+np=2
+hbar=1.054571817e-34
+eps=8.8541878128e-12
+c=2.99792458e8
+mu=2.54e-29
+lamda0=1e-6
+k0=2*pi/lamda0
+w=k0*c
+
+space1=NLevelBasis(N)
+fock=bosonstates(space1,np)
+onehot=ManyBodyBasis(space1,fock)
+dimension=length(fock)
+Id=identityoperator(onehot)
+
+H=zeros(Float64,N,N)
+r=zeros(Float64,N,N)
+ege=10
+Gamma=zeros(Float64,N,N)
+Omega=zeros(Float64,N,N)
+reeig1=zeros(Float64,ege)
+reeig2=zeros(Float64,ege)
+reeig3=zeros(Float64,ege)
+imeig1=zeros(Float64,ege)
+imeig2=zeros(Float64,ege)
+imeig3=zeros(Float64,ege)
+lvec=zeros(Float64,ege)
+
+for l=1:ege
+    global H
+    cosofangle=-1+(l*0.1)
+    for i=1:N
+        global H
+        for j=1:N
+            global H
+            ri=0.1*lamda0*i
+            rj=0.1*lamda0*j
+            r[i,j]=abs(rj-ri)
+            mudotr=cosofangle
+            if i==j
+            Gamma[i,j]=((w^3)*(mu^2))/(3*pi*eps*hbar*(c)^3)
+            mudotr=0
+            end
+            if i!=j  
+            mudotr=cosofangle
+            Omega[i,j]=(3/4)*((w^3)*(mu^2))/(3*pi*eps*hbar*(c)^3)*(((-(1-mudotr^2)*cos(k0*r[i,j]))/(k0*r[i,j]))+((1-3*mudotr^2)*((sin(k0*r[i,j]))/(k0*r[i,j])^2+cos(k0*r[i,j])/(k0*r[i,j])^3)))
+            Gamma[i,j]=(3/2)*((w^3)*(mu^2))/(3*pi*eps*hbar*(c)^3)*((1-mudotr^2)*(sin(k0*r[i,j])/(k0*r[i,j]))+(1-3*mudotr^2)*(cos(k0*r[i,j])/(k0*r[i,j])^2-sin(k0*r[i,j])/(k0*r[i,j])^3))
+            end
+        end
+    end
+    H=(-im/2)*Gamma+Omega
+    Energy=eigvals(H)
+    reeig1[l]=real(Energy[1])
+    reeig2[l]=real(Energy[2])
+    reeig3[l]=real(Energy[3])
+    imeig1[l]=imag(Energy[1])
+    imeig2[l]=imag(Energy[2])
+    imeig3[l]=imag(Energy[3])
+    lvec[l]=l
+    println(Energy)
+end
+
+p1 = plot(lvec, [reeig1 reeig2 reeig3],
+          label=["Re E1" "Re E2" "Re E3"],
+          title="Real eigenvalues")
+
+p2 = plot(lvec, [imeig1 imeig2 imeig3],
+          label=["Im E1" "Im E2" "Im E3"],
+          title="Imag eigenvalues")
+
+p = plot(p1, p2, layout=(2,1), size=(800,600))
+
+display(p)
+
+readline()
+
---
+++ Crosspiover4.m
@@ -0,0 +1,81 @@
+clc;
+clear all;
+close all;
+
+%S=((sin(teta)*cos(-r/c))/r)^2;
+%E=((sin(teta)*cos(-r/c))/r)^2 theta;
+%B=((sin(teta)*cos(-r/c))/r)^2 phi;
+lamda=6*pi*10^(-6);
+R=100*lamda;
+k=(2*pi)/lamda;
+i=1;
+C=1.4*10^(-19);
+Evec=0;
+Bvec=0;
+rot=[1/sqrt(2) 0 -1/sqrt(2);
+         0         1 0;
+         1/sqrt(2) 0  1/sqrt(2)];
+         
+
+for teta=0:0.01:pi
+    for phi=0:0.01:2*pi
+        posS=[R*sin(teta)*cos(phi),R*sin(teta)*sin(phi),R*cos(teta)];
+            absR1=posS-[0,0,0];
+            d1=sqrt(sum(absR1.^2));
+            teta1=acos(absR1(3)/d1);
+            phi1=atan2(absR1(2),absR1(1));
+            E1=((sin(teta1)*cos(-d1*k))/d1);
+            B1=((sin(teta1)*cos(-d1*k))/d1);
+            E1vec=[E1*cos(teta1)*cos(phi1),E1*cos(teta1)*sin(phi1),-E1*sin(teta1)];
+            B1vec=[-B1*sin(phi1),B1*cos(phi1),0];
+            Evec=Evec+E1vec;
+            Bvec=Bvec+B1vec;
+        for d=0:0.4*lamda:lamda*2
+            posS=[R*sin(teta)*cos(phi),R*sin(teta)*sin(phi),R*cos(teta)];
+            absR1=posS-[d/(2*sqrt(2)),0,d/(2*sqrt(2))];
+            absR2=posS-[-d/(2*sqrt(2)),0,-d/(2*sqrt(2))];
+            d1=sqrt(sum(absR1.^2));
+            d2=sqrt(sum(absR2.^2));
+            teta1=acos(absR1(3)/d1);
+            teta2=acos(absR2(3)/d2);
+            phi1=atan2(absR1(2),absR1(1));
+            phi2=atan2(absR2(2),absR2(1));
+            E1=((sin(teta1)*cos(-d1*k))/d1);
+            E2=((sin(teta2)*cos(-d2*k))/d2);
+            B1=((sin(teta1)*cos(-d1*k))/d1);
+            B2=((sin(teta2)*cos(-d2*k))/d2);
+            E1vec=[E1*cos(teta1)*cos(phi1),E1*cos(teta1)*sin(phi1),-E1*sin(teta1)];
+            E2vec=[E2*cos(teta2)*cos(phi2),E2*cos(teta2)*sin(phi2),-E2*sin(teta2)];
+            B1vec=[-B1*sin(phi1),B1*cos(phi1),0];
+            B2vec=[-B2*sin(phi2),B2*cos(phi2),0];
+            Evec=Evec+E1vec+E2vec;
+            Bvec=Bvec+B1vec+B2vec;
+            Svec=cross(Evec,Bvec);
+        end
+        S=C*sqrt(sum(Svec.^2));
+            Sshow(i)=S;
+            tetavec(i)=teta;
+            phivec(i)=phi;
+            i=i+1;  
+            Evec=0;
+            Bvec=0;
+    end
+end
+
+x=R*sin(tetavec).*cos(phivec);
+y=R*sin(tetavec).*sin(phivec);
+z=R*cos(tetavec);
+
+coords = rot * [x; y; z];
+
+x = coords(1,:);
+y = coords(2,:);
+z = coords(3,:);
+
+figure;
+scatter3(x,y,z,15,log10(Sshow),'filled');
+colorbar;
+axis equal;
+xlabel('x'); ylabel('y'); zlabel('z');
+title('Destructive interference points');
+grid on;
